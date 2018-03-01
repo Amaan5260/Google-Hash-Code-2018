17bfadeb659ce04ec1dd0fe0a9c96c50328d2f6b
Kavan Price <sc17k2p@leeds.ac.uk>
2018-03-01 18:28:08 +0000

    Test push

---
+++ Ride.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Ride {
+    private int startx;
+    private int starty;
+    private int finishx;
+    private int finishy;
+    private int earliestStart;
+    private int latestFinish;
+    
+    
+}
